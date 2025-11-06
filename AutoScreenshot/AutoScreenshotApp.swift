b83c9fb6774d7d6243a92808ce96e44d44a2f5f8
nuss <andreasnuss99@gmail.com>
2025-11-06 22:26:06 +0100

    Initial Commit

---
+++ AutoScreenshot/AutoScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScreenshotApp.swift
+//  AutoScreenshot
+//
+//  Created by Andreas Nuss on 06.11.25.
+//
+
+import SwiftUI
+
+@main
+struct AutoScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
